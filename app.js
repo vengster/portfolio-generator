2b2f846d294e5d0e7ed93bdb0dbb901205ff8215
vengster <veng.lee@gmail.com>
2022-06-27 23:03:44 -0700

    9.3.5

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const generatePage = require('./src/page-template.js');
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-
-  fs.writeFile('./index.html', generatePage(name, github), err =>{
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+import inquirer from 'inquirer'
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const pageHTML = generatePage(name, github);
+//   fs.writeFile('./index.html', pageHTML, err =>{
+//     if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
   
